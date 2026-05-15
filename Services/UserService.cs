9e3af4436ce696d91eb196a0459315e2f78ef9d8
Mateus Torres <mmtorres230726@gmail.com>
2026-05-15 16:50:55 -0300

    feat: Implementing the user service method for visualizing the users and their data and the method for changing role

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,6 +1,49 @@
-﻿namespace Services;
+﻿using Infra;
+using Domain.Models;
+using Domain.DTOs.Store;
+using Domain.DTOs.User;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services;
 
 public class UserService
 {
+    private readonly AppDbContext _dbContext;
     
+    public UserService(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<List<UserInfo>> ListUsers()
+    {
+        return await _dbContext.Users
+            .Select(u => new UserInfo(u.Id, u.Email))
+            .ToListAsync();
+    }
+    
+    public async Task<UserInfoDetails> GetUserDetails(Guid userId)
+    {
+        var user = await _dbContext.Users.SingleOrDefaultAsync(u => u.Id == userId)
+            ?? throw new ArgumentException($"User with ID {userId} not found");
+        
+        List<StoreInfo> userStores = await _dbContext.Stores
+            .Where(s => s.OwnerId == userId)
+            .Select(s => new StoreInfo(s.Id, s.Name))
+            .ToListAsync();
+
+        return new UserInfoDetails(user.Id, user.Email, user.Role, userStores);
+    }
+
+    public async Task<UpgradeRoleResponse> UpgradeRole (UpgradeRoleRequest request)
+    {
+        User user = await _dbContext.Users.SingleOrDefaultAsync(u => u.Id == request.userId)
+            ?? throw new ArgumentException($"User with id {request.userId} not found");
+        
+        user.Role = request.Role;
+        
+        await _dbContext.SaveChangesAsync();
+
+        return new UpgradeRoleResponse(user.Email, user.Role);
+    }
 }
