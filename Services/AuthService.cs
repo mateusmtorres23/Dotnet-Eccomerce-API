ad318d25e418ceeeabd4185a3c6863692a58eb50
Mateus Torres <mmtorres230726@gmail.com>
2026-05-15 12:54:57 -0300

    feat: Implementing the AuthService methods for register and login and the PasswordHasher for security purposes

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,6 +1,62 @@
-﻿namespace Services;
+﻿using Infra;
+using Domain;
+using Domain.DTOs;
+using Domain.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services;
 
 public class AuthService
 {
+    private readonly AppDbContext _dbContext;
+    private readonly IPasswordHasher _passwordHasher;
+    private readonly ITokenService _tokenService;
     
+    public AuthService(AppDbContext dbContext, IPasswordHasher passwordHasher, ITokenService tokenService)
+    {
+        _dbContext = dbContext;
+        _passwordHasher = passwordHasher;
+        _tokenService = tokenService;
+    }
+
+    public async Task<RegisterResponse> RegisterUser(RegisterRequest request)
+    {
+        string passwordHash = _passwordHasher.Hash(request.Password);
+
+        User newUser = new User
+        {
+            Id = Guid.NewGuid(),
+            Email = request.Email,
+            Password = passwordHash,
+            Role = UserRole.Customer
+        };
+        
+        _dbContext.Users.Add(newUser);
+
+        try
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+        catch (DbUpdateException ex) when (ex.InnerException?.Message.Contains("23505") ==  true)
+        {
+            throw new DuplicateEmailException("Email already in use.");
+        }
+        
+        return new RegisterResponse{ Email = request.Email };
+    }
+
+    public async Task<LoginResponse> LoginUser(LoginRequest request)
+    {
+        User user  = await _dbContext.Users.SingleOrDefaultAsync(x => x.Email == request.Email)
+            ?? throw new Exception("Invalid email or password.");
+
+        if (!_passwordHasher.Verify(request.Password, user.Password))
+        {
+            throw new Exception("Invalid email or password.");
+        }
+
+        string token = _tokenService.GenerateToken(user);
+        
+        return new LoginResponse { Token = token };
+    }
 }
--- Services/PasswordHasher.cs
+++ Services/PasswordHasher.cs
@@ -1,6 +1,25 @@
-﻿namespace Services;
+﻿using BCrypt.Net;
 
-public class PasswordHasher
+namespace Services;
+
+public interface IPasswordHasher
 {
+    string Hash(string password);
+    bool Verify(string password, string hashedStrign);
+}
+
+public class PasswordHasher : IPasswordHasher
+{
+    private const int workFactor = 12;
+
+    public string Hash(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password, workFactor);
+    }
+
+    public bool Verify(string password, string hashedString)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hashedString);
+    }
     
 }
