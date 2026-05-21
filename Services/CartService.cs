ec75ba5dcc51320b3e0193347af586957d53e538
Mateus Torres <mmtorres230726@gmail.com>
2026-05-21 11:46:04 -0300

    feat: Implementing the service methods for the cart module

---
+++ Services/CartService.cs
@@ -0,0 +1,54 @@
+﻿using Domain.DTOs.Cart;
+using Domain.Models;
+using Infra;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services;
+
+public class CartService
+{
+    private readonly AppDbContext _dbContext;
+    
+    public CartService(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<AddProductCartResponse> AddProductCart(AddProductCartRequest request, Guid userId)
+    {
+        Product product = await _dbContext.Products.FindAsync(request.ProductId)
+            ?? throw new NullReferenceException("Product not found");
+        
+        CartItem newCartItem = new CartItem
+        {
+            Id = Guid.NewGuid(),
+            ProductId = request.ProductId,
+            UserId = userId
+        };
+        
+        _dbContext.CartItems.Add(newCartItem);
+        await _dbContext.SaveChangesAsync();
+
+        return new AddProductCartResponse(product.Name, product.Price);
+    }
+    
+    public async Task RemoveProductCart(RemoveProductCartRequest request,  Guid userId)
+    {
+        CartItem cartItem = await _dbContext.CartItems.FirstOrDefaultAsync(ci => 
+                                ci.ProductId == request.ProductId && ci.UserId == userId)
+            ?? throw new NullReferenceException("Cart item not found");
+    }
+    
+    public async Task CheckoutCart(Guid userId)
+    {
+            List<CartItem> cartItems = await _dbContext.CartItems.Where(ci => ci.UserId == userId).ToListAsync();
+            
+            if (!cartItems.Any())
+            {
+                throw new InvalidOperationException("Cart is empty");
+            }
+            
+            _dbContext.CartItems.RemoveRange(cartItems);
+            await _dbContext.SaveChangesAsync();
+    }
+}
